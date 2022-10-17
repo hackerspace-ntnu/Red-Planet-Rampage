68219fa31e45a8a80837cb55df14b46115042438
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2022-10-18 00:58:47 +0200

    Updated menu scene and added handling of multiple players
    - Updated canvas in scene with a view for displaying amount of players
    - Updated scripts to create, rezise and display a panel for each player

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -6,12 +6,18 @@ using UnityEngine.InputSystem;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject MainMenuWrapper;
+    private GameObject mainMenuWrapper;
     [SerializeField]
-    private GameObject CharacterMenuWrapper;
+    private GameObject characterMenuWrapper;
     [SerializeField]
-    private GameObject CharacterView;
+    private RectTransform characterView;
     private PlayerInputManager playerInputManager;
+
+    [SerializeField]
+    private GameObject playerBackgroundPanel;
+
+    private List<PlayerInput> playerInputs = new List<PlayerInput>();
+    private List<GameObject> playerBackgrounds = new List<GameObject>();
 
     void Start()
     {
@@ -19,20 +25,28 @@ public class MainMenuController : MonoBehaviour
         playerInputManager.onPlayerJoined += AddPlayer;
     }
 
+    private void OnDestroy()
+    {
+        playerInputManager.onPlayerJoined -= AddPlayer;
+    }
+
     public void ToggleMenu()
     {
-        MainMenuWrapper.SetActive(!MainMenuWrapper.activeSelf);
-        CharacterMenuWrapper.SetActive(!CharacterMenuWrapper.activeSelf);
+        mainMenuWrapper.SetActive(!mainMenuWrapper.activeSelf);
+        characterMenuWrapper.SetActive(!characterMenuWrapper.activeSelf);
     }
 
     private void AddPlayer(PlayerInput playerInput)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        playerInputs.Add(playerInput);
+        var width = characterView.rect.width/playerInputs.Count;
+        GameObject panel = Instantiate(playerBackgroundPanel, characterView);
+        playerBackgrounds.Add(panel);
+        for (int i=0; i<playerBackgrounds.Count; i++)
+        {
+            playerBackgrounds[i].transform.LeanMoveLocalX(width * i, 0.25f);
+            var rectTransform = playerBackgrounds[i].GetComponent<RectTransform>();
+            rectTransform.sizeDelta = new Vector2(width, rectTransform.sizeDelta.y);
+        }
     }
 }
