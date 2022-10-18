26ae51a65de717de1507fcc4e675c238f9234a9c
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2022-10-18 15:49:52 +0200

    Fixed button selection between different menus
    - Navigation between menus can now be defined in onclick events on buttons
    - Updated Menu scene to have proper navigation between menus with new system

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -11,11 +13,10 @@ public class MainMenuController : MonoBehaviour
     private GameObject characterMenuWrapper;
     [SerializeField]
     private RectTransform characterView;
-    private PlayerInputManager playerInputManager;
-
     [SerializeField]
     private GameObject playerBackgroundPanel;
 
+    private PlayerInputManager playerInputManager;
     private List<PlayerInput> playerInputs = new List<PlayerInput>();
     private List<GameObject> playerBackgrounds = new List<GameObject>();
 
@@ -30,12 +31,29 @@ public class MainMenuController : MonoBehaviour
         playerInputManager.onPlayerJoined -= AddPlayer;
     }
 
+    /// <summary>
+    /// Call this function for selecting a specific button in scene.
+    /// Typically used for jumping to a new button with onclick after toggleMenu()
+    /// </summary>
+    /// <param name="target"></param>
+    public void SelectButton(Button target)
+    {
+        StartCoroutine(WaitSelect(target));
+    }
+
+    /// <summary>
+    /// Toggles between mainMenu and characterMenu.
+    /// </summary>
     public void ToggleMenu()
     {
         mainMenuWrapper.SetActive(!mainMenuWrapper.activeSelf);
         characterMenuWrapper.SetActive(!characterMenuWrapper.activeSelf);
     }
 
+    /// <summary>
+    /// Subscribes to onplayerjoined and is responsible for adapting menu to new player inputs.
+    /// </summary>
+    /// <param name="playerInput"></param>
     private void AddPlayer(PlayerInput playerInput)
     {
         playerInputs.Add(playerInput);
@@ -48,5 +66,12 @@ public class MainMenuController : MonoBehaviour
             var rectTransform = playerBackgrounds[i].GetComponent<RectTransform>();
             rectTransform.sizeDelta = new Vector2(width, rectTransform.sizeDelta.y);
         }
+    }
+
+    //Unity for some reason can't immediately Select() button after an onclick event, so this helper function skips a single frame to do so.
+    private IEnumerator WaitSelect(Button target)
+    {
+        yield return null;
+        target.Select();
     }
 }
