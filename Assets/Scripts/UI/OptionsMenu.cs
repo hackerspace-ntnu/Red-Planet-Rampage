6a9d2a54d7cf44a491f8f2a8b972d7eee4db9b36
Thomas Jakob Gabrielsen <thomajga@stud.ntnu.no>
2022-11-30 00:55:16 +0100

    Issue fix
    fixed issue where sliders are deselected when they are selected in
    the Menu. All future sliders must also have the SliderFocus.cs script
    attached

--- Assets/Scripts/UI/OptionsMenu.cs
+++ Assets/Scripts/UI/OptionsMenu.cs
@@ -1,11 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class OptionsMenu : MonoBehaviour
 {
