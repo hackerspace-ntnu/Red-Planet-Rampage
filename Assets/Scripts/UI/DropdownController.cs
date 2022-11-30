afde6bd6f5668af07f26b72ea4d77fb94398f98c
Thomas Jakob Gabrielsen <thomajga@stud.ntnu.no>
2022-11-30 14:42:44 +0100

    Fixed "nitpicks"

--- Assets/Scripts/UI/DropdownController.cs
+++ Assets/Scripts/UI/DropdownController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// Allows for our controllers to freely act in dropdown menus
+/// </summary>
 public class DropdownController : MonoBehaviour, ISelectHandler
 {
     private ScrollRect scrollRect;
     private float scrollPosition;
 
-    // Start is called before the first frame update
     void Start()
     {
         scrollRect = GetComponentInParent<ScrollRect>(true);
@@ -17,6 +19,7 @@ public class DropdownController : MonoBehaviour, ISelectHandler
         int childCount = scrollRect.content.childCount - 1;
         int childIndex = transform.GetSiblingIndex();
 
+        // Slightly adjusts the child index to prevent ugly cutoff when at the extremities.
         childIndex = childIndex < ((float)childCount / 2f) ? childIndex - 1 : childIndex;
 
         scrollPosition = 1 - ((float)childIndex / childCount);
@@ -26,10 +29,5 @@ public class DropdownController : MonoBehaviour, ISelectHandler
     {
         if(scrollRect)
             scrollRect.verticalScrollbar.value = scrollPosition;
-    }
-
-    public void Changed()
-    {
-        Debug.Log("Value changed");
     }
 }
