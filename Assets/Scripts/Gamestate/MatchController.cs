77ea90747d917746159528fc74a5014dda840b56
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2022-11-30 01:20:37 +0100

    Round timer works visually and functionally
    - Implemented HUD that is shared among every player (global)
    - Updated Timer to be able to auto update text whever relevant
    - Made TMP text be a bit bolder by giving it an outline
    - Round can now be ended by time running out (no winner)
    - Timer HUD is placed differently depending on no. of players

--- Assets/Scripts/Gamestate/MatchController.cs
+++ Assets/Scripts/Gamestate/MatchController.cs
@@ -33,6 +33,9 @@ public class MatchController : MonoBehaviour
     public MatchEvent onBiddingStart;
     public MatchEvent onBiddingEnd;
 
+    [SerializeField]
+    private float roundStartTime;
+
     [Header("Chip rewards")]
     [SerializeField]
     private int startAmount = 0;
@@ -43,7 +46,10 @@ public class MatchController : MonoBehaviour
     [SerializeField]
     private int rewardBase = 1;
 
-    public Timer timer;
+    public Timer roundTimer;
+
+    [SerializeField]
+    private HUDGlobalController globalHud;
 
     private List<Player> players = new List<Player>();
     private List<Round> rounds = new List<Round>();
@@ -83,6 +89,9 @@ public class MatchController : MonoBehaviour
     {
         onRoundStart?.Invoke();
         rounds.Add(new Round(players.Select(player => player.playerManager).ToList()));
+        roundTimer.StartTimer(roundStartTime);
+        roundTimer.OnTimerUpdate += HUDTimerUpdate;
+        roundTimer.OnTimerRunCompleted += EndActiveRound;
     }
 
     public void StartNextBidding()
@@ -95,7 +104,8 @@ public class MatchController : MonoBehaviour
     public void EndActiveRound()
     {
         onRoundEnd?.Invoke();
-
+        roundTimer.OnTimerUpdate -= HUDTimerUpdate;
+        roundTimer.OnTimerRunCompleted -= EndActiveRound;
         AssignRewards();
 
         if (!IsWin())
@@ -128,9 +138,15 @@ public class MatchController : MonoBehaviour
         }
     }
 
+    private void HUDTimerUpdate()
+    {
+        globalHud.OnTimerUpdate(roundStartTime - roundTimer.ElapsedTime);
+    }
+
     private bool IsWin()
     {
         var lastWinner = rounds.Last().Winner;
+        if (lastWinner == null) { return false; }
         var wins = rounds.Where(round => round.IsWinner(lastWinner)).Count();
         Debug.Log("Current winner (" + lastWinner.ToString() + ") has " + wins + " wins.");
         if (wins >= 3)
---
+++ Assets/Scripts/UI/HUDGlobalController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HUDGlobalController : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text roundTimer;
+    [SerializeField]
+    private float textOffsetY = 180f;
+
+    void Start()
+    {
+        if (PlayerInputManagerController.Singleton.playerInputs.Count > 2)
+        {
+            roundTimer.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        }
+        else
+        {
+            roundTimer.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, textOffsetY);
+        }
+    }
+
+    public void OnTimerUpdate(float time)
+    {
+        int seconds = Mathf.FloorToInt(time % 60);
+        roundTimer.text = Mathf.FloorToInt(time / 60) + ":" + (seconds<10 ? "0" : "") + seconds;
+    }
+}
