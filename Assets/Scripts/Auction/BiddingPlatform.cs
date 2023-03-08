e1b76e3ce36eed8fbb23be9c2ea706720b37ea20
Tore Bergebakken <toberge@stud.ntnu.no>
2023-03-08 19:57:28 +0100

    Add leading bidder indicator to bidding platforms
    - Replace bidding platform model with one with more suitable UV mapping
    - Add material that displays a border in the leading bidder's color

--- Assets/Scripts/Auction/BiddingPlatform.cs
+++ Assets/Scripts/Auction/BiddingPlatform.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Timer))]
@@ -41,13 +38,17 @@ public class BiddingPlatform : MonoBehaviour
     [SerializeField]
     private Timer auctionTimer;
 
+    [SerializeField]
+    private float borderTweenDuration = 0.2f;
+
+    private Material material;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out PlayerManager playerManager))
         {
             playerManager.SelectedBiddingPlatform = this;
         }
-
     }
 
     private void OnTriggerExit(Collider other)
@@ -64,6 +65,8 @@ public class BiddingPlatform : MonoBehaviour
         auctionTimer = GetComponent<Timer>();
         auctionTimer.OnTimerUpdate += UpdateTimer;
         auctionTimer.OnTimerRunCompleted += EndAuction;
+        material = GetComponent<MeshRenderer>().material;
+        material.SetFloat("_Scale", 0f);
     }
 
     private void OnDestroy()
@@ -92,12 +95,19 @@ public class BiddingPlatform : MonoBehaviour
             playerIdentity.UpdateChip(-chips);
             itemCostText.text = chips.ToString();
             leadingBidder = playerIdentity;
+            material.SetColor("_BidderColor", playerIdentity.color);
+            LeanTween.value(gameObject, UpdateBorder, 0f, 1f, borderTweenDuration);
 
             if ((auctionTimer.WaitTime - auctionTimer.ElapsedTime) < bumpTime) { auctionTimer.AddTime(bumpTime); }
 
             return true;
         }
         return false;
+    }
+
+    private void UpdateBorder(float scale)
+    {
+        material.SetFloat("_Scale", scale);
     }
 
     private void UpdateTimer()
@@ -108,7 +118,10 @@ public class BiddingPlatform : MonoBehaviour
     private void EndAuction()
     {
         if (leadingBidder)
+        {
             leadingBidder.PerformTransaction(item);
+            LeanTween.value(gameObject, UpdateBorder, 1f, 0f, borderTweenDuration);
+        }
 
         //TODO: Remove this, call from auction driver or matchmanager
         StartCoroutine(MatchController.Singleton.WaitAndStartNextRound());
