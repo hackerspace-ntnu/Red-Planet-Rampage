2cb9b3082853ff2d7cda7f7ff72e76d1ba63b9a6
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-03-23 02:40:02 +0100

    Added the Fire extension augment
    - OnHit implementation not yet added, as we are awaiting some other changes

---
+++ Assets/Scripts/Augment/AugmentImplementations/Fire.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : GunExtension
+{
+    [SerializeField]
+    private GameObject fire;
+
+    private GunController gunController;
+
+    void Awake()
+    {
+        gunController = transform.parent.GetComponent<GunController>();
+        if (!gunController)
+        {
+            Debug.Log("Fire not attached to gun parent!");
+            return;
+        }
+        gunController.onInitialize += AddFireToProjectile;
+    }
+
+    private void AddFireToProjectile(GunStats gunstats)
+    {
+        Instantiate(fire, gunController.projectile.transform);
+    }
+}
