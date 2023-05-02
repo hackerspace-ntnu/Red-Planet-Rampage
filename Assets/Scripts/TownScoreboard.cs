3a453f07f057aeaf79c646f8a9e6287632b2299e
Thomas J. Gabrielsen <thomajga@stud.ntnu.no>
2023-05-02 17:39:36 +0200

    Reformatting code

--- Assets/Scripts/TownScoreboard.cs
+++ Assets/Scripts/TownScoreboard.cs
@@ -1,95 +1,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System;
 using Unity.VisualScripting;
 
-[Serializable]
-public struct Poster
-{
-    public Player player;
-    public WantedPoster wantedPoster;
-
-    public Poster(Player Player, WantedPoster WantedPoster)
-    {
-        player = Player;
-        wantedPoster = WantedPoster;
-    }
-}
 public class TownScoreboard : MonoBehaviour
 {
     private MatchController matchController;
     private List<Player> players;
 
     public GameObject wantedPosterPrefab;
-    private Poster[] posters;
-
-    public GameObject crimeTextPrefab;
+    private WantedPoster[] posters;
 
     [SerializeField]
-    private List<string> wantedSubtitles= new List<string>();
+    private List<string> wantedSubtitles = new List<string>();
 
     void Start()
     {
         matchController = MatchController.Singleton;
         players = matchController.Players;
-        posters = new Poster[players.Count];
+        posters = new WantedPoster[players.Count];
 
         for (int i = 0; i < players.Count; i++)
         {
-            posters[i] = new Poster(players[i], Instantiate(wantedPosterPrefab, transform).GetComponentInChildren<WantedPoster>());
-
-            posters[i].wantedPoster.HistoryValue.text = posters[i].player.playerIdentity.chips.ToString();
-            posters[i].wantedPoster.Subtitle.text = wantedSubtitles[UnityEngine.Random.Range(0, wantedSubtitles.Count)];
-
-            posters[i].wantedPoster.TotalValue.text = players[i].playerIdentity.chips.ToString();
+            posters[i] = Instantiate(wantedPosterPrefab, transform).GetComponentInChildren<WantedPoster>();
+            posters[i].SetupPoster(players[i], matchController, wantedSubtitles[Random.Range(0, wantedSubtitles.Count - 1)]);
+            posters[i].gameObject.SetActive(false);
         }
 
         matchController.onRoundEnd += UpdateScoreboard;
     }
 
-    public void AddPosterCrime(Player player, string Crime, int Value)
-    {
-        Poster poster = GetPlayerPoster(player);
-        
-        GameObject crime = Instantiate(crimeTextPrefab, poster.wantedPoster.CrimeContent);
-        TMP_Text[] texts = crime.GetComponentsInChildren<TMP_Text>();
-
-        texts[0].text = Crime;
-        texts[1].text = Value.ToString();
-    }
-
-    private Poster GetPlayerPoster(Player player)
-    {
-        for (int i = 0; i < players.Count; i++)
-        {
-            if (player == posters[i].player)
-                return posters[i];
-        }
-        Debug.LogError("Unable to find poster connected to player.");
-        return posters[0];
-    }
-
     public void UpdateScoreboard()
     {
-        var lastRound = matchController.GetLastRound();
-        
-        foreach (var player in players)
+        // Animate the after battle scene
+        Camera.main.GetComponent<Animator>().SetTrigger("ScoreboardZoom");
+
+        for (int i = 0; i < posters.Length; i++)
         {
-            AddPosterCrime(player, "Base reward", matchController.ChipBaseReward);
-            AddPosterCrime(player, "Kills", matchController.ChipKillReward * lastRound.KillCount(player.playerManager));
+            // Disable player camera
+            players[i].playerManager.inputManager.GetComponent<Camera>().enabled = false;
 
-            if(lastRound.IsWinner(player.playerIdentity))
-            {
-                AddPosterCrime(player, "Victor", matchController.ChipWinReward);
-            }
+            // Enable posters
+            posters[i].gameObject.SetActive(true);
 
-            GetPlayerPoster(player).wantedPoster.TotalValue.text = player.playerIdentity.chips.ToString();
-
-            // Animate the after battle scene
-            Camera.main.GetComponent<Animator>().SetTrigger("ScoreboardZoom");
-
-            player.playerManager.inputManager.GetComponent<Camera>().enabled = false;
+            posters[i].UpdatePosterValues();
         }
     }
 }
--- Assets/Scripts/UI/WantedPoster.cs
+++ Assets/Scripts/UI/WantedPoster.cs
@@ -8,28 +8,79 @@ public class WantedPoster : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text title;
-    public TMP_Text Title => title;
 
     [SerializeField]
     private TMP_Text subtitle;
-    public TMP_Text Subtitle => subtitle;
 
     public Image photo;
 
     [SerializeField]
-    private TMP_Text history;
-    public TMP_Text History => history;
-
-    [SerializeField]
-    private TMP_Text historyValue;
-    public TMP_Text HistoryValue => historyValue;
+    private TMP_Text savingsValue;
 
     [SerializeField]
     private Transform crimeContent;
-    public Transform CrimeContent => crimeContent;
 
     [SerializeField]
     private TMP_Text totalValue;
-    public TMP_Text TotalValue => totalValue;
 
+    [SerializeField]
+    private GameObject crimeTextPrefab;
+
+    private Player player;
+    private MatchController matchController;
+    private int roundSpoils;
+
+    public void SetupPoster(Player player, MatchController matchController, string subtitle)
+    {
+        this.player = player;
+        this.matchController = matchController;
+        this.subtitle.text = subtitle;
+    }
+
+    private void AddPosterCrime(string Crime, int Value)
+    {
+        GameObject crime = Instantiate(crimeTextPrefab, crimeContent);
+        TMP_Text[] texts = crime.GetComponentsInChildren<TMP_Text>();
+
+        texts[0].text = Crime;
+        texts[1].text = Value.ToString();
+    }
+
+    public void UpdatePosterValues()
+    {
+        if(matchController == null)
+            matchController = MatchController.Singleton;
+
+        Round lastRound = matchController.GetLastRound();
+        int 
+        savingsValue.text = player.playerIdentity.chips.ToString();
+
+        AddPosterCrime("Base", matchController.ChipBaseReward);
+        AddPosterCrime("Kills", matchController.ChipKillReward * lastRound.KillCount(player.playerManager));
+
+        if (lastRound.IsWinner(player.playerIdentity)){
+            AddPosterCrime("Victor", matchController.ChipWinReward);
+        }
+
+        roundSpoils = matchController.ChipBaseReward 
+            + matchController.ChipKillReward * lastRound.KillCount(player.playerManager) 
+            + (lastRound.IsWinner(player.playerIdentity) ? matchController.ChipWinReward : 0);
+
+        LeanTween.value(totalValue.gameObject, RandomNumber, 0, player.playerIdentity.chips + roundSpoils, 5);
+    }
+
+    private void RandomNumber(float number)
+    {
+        if (number == player.playerIdentity.chips + roundSpoils)
+        {
+            totalValue.text = (player.playerIdentity.chips + roundSpoils).ToString("F0");
+        }           
+        else 
+        {
+            //totalValue.text = number.ToString("F0");
+            totalValue.text = Random.Range(0, 99).ToString("F0");
+        }
+        //
+    }
 }
+
