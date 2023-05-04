4438a2da1840820d467e8f9f459b4df49b646279
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-05-04 15:24:38 +0200

    Add ddrBody prefab and unifinished screen shader material

---
+++ Assets/Scripts/Augment/AugmentImplementations/DDRBody.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDRBody : GunBody
+{
+
+    protected override void Reload(GunStats stats)
+    {
+        base.Reload(stats);
+    }
+
+}
