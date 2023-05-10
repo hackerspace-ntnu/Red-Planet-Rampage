9868cc87c4c33dc90af91af5e2c0cd8a348dd677
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-05-10 23:53:34 +0200

    Add text feedback and cleaner code in dance body
    - Made arrows flash, then reset after input
    - Made feedback text pop up after succesful input
    - Renamed ddr files to be PascalCase

--- Assets/Scripts/Augment/AugmentImplementations/DDRBody.cs
+++ Assets/Scripts/Augment/AugmentImplementations/DDRBody.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[System.Serializable]
+public struct Precision
+{
+    public float range;
+    public string text;
+    public Color color;
+    public float awardFactor;
+}
 public class DDRBody : GunBody
 {
     [SerializeField]
     private SkinnedMeshRenderer meshRenderer;
+
+    [SerializeField]
+    private TMP_Text precisionText;
 
     private Material ddrMaterial;
 
@@ -20,19 +32,22 @@ public class DDRBody : GunBody
     private const float errorMarginInput = 0.1f;
 
     private const float targetPoint = 3;
-    private const float rangeMeh = 0.5f;
-    private const float rangeGood = 0.25f;
-    private const float rangeGreat = 0.1f;
-    private const float rangeExcellent = 0.05f;
 
     [SerializeField]
-    private float secondPerBeat = 1;
+    private Precision[] precisionsGoodToBad;
+
+    LTDescr arrowMover;
+
+    [SerializeField]
+    private float secondPerArrow = 1;
+    private float musicPaces = 0.4285714f;
 
 
     public override void Start()
     {
         meshRenderer.materials[screenMaterialIndex] = Instantiate(meshRenderer.materials[screenMaterialIndex]);
         ddrMaterial = meshRenderer.materials[screenMaterialIndex];
+        precisionText.enabled = false;
 
         gunController = transform.parent.GetComponent<GunController>();
         if (!gunController)
@@ -44,30 +59,43 @@ public class DDRBody : GunBody
         if (gunController.player)
         {
             gunController.player.inputManager.onMovePerformed += ArrowSelect;
-            ResetArrow();
-            LeanTween.value(gameObject, SetArrowHeigth, 0, screenHeigth, secondPerBeat)
+            arrowMover = LeanTween.value(gameObject, SetArrowHeigth, 0, screenHeigth, secondPerArrow)
                 .setRepeat(-1)
-                .setOnComplete(() => ResetArrow());
+                .setOnComplete(ResetArrow);
+
+            LeanTween.value(gameObject, SetBackgroundZoom, 0.5f, 1.5f, musicPaces)
+                .setLoopPingPong();
         }
-        
+
     }
 
     protected override void Reload(GunStats stats)
     {
-        float danceAccuracy = 0;
-        if (Mathf.Abs(arrowHeigth - targetPoint) <= rangeExcellent)
-            danceAccuracy = 1;
-        else if(Mathf.Abs(arrowHeigth - targetPoint) <= rangeGreat)
-            danceAccuracy = 0.75f;
-        else if (Mathf.Abs(arrowHeigth - targetPoint) <= rangeGood)
-            danceAccuracy = 0.5f;
-        else if (Mathf.Abs(arrowHeigth - targetPoint) <= rangeMeh)
-            danceAccuracy = 0.25f;
+        Precision? precision = null;
 
-        if (danceAccuracy == 0)
+        if (Mathf.Abs(arrowHeigth - targetPoint) <= precisionsGoodToBad[0].range)
+            precision = precisionsGoodToBad[0];
+        else if (Mathf.Abs(arrowHeigth - targetPoint) <= precisionsGoodToBad[1].range)
+            precision = precisionsGoodToBad[1];
+        else if (Mathf.Abs(arrowHeigth - targetPoint) <= precisionsGoodToBad[2].range)
+            precision = precisionsGoodToBad[2];
+        else if (Mathf.Abs(arrowHeigth - targetPoint) <= precisionsGoodToBad[3].range)
+            precision = precisionsGoodToBad[3];
+
+        if (!precision.HasValue)
             return;
 
-        gunController.Reload(reloadEfficiencyPercentage*danceAccuracy);
+        precisionText.enabled = true;
+        precisionText.text = precision.Value.text;
+        precisionText.color = precision.Value.color;
+        precisionText.gameObject.LeanScale(new Vector3(1.1f, 1.1f, 1.1f), 0.5f)
+            .setEasePunch()
+            .setOnComplete(
+            () => precisionText.enabled = false);
+            
+        LeanTween.value(gameObject, SetFlashFactor, 0, 20f, 0.5f).setEasePunch();
+
+        gunController.Reload(reloadEfficiencyPercentage*precision.Value.awardFactor);
     }
 
     private void ArrowSelect(InputAction.CallbackContext ctx)
@@ -93,6 +121,16 @@ public class DDRBody : GunBody
         }
 
         Reload(gunController.stats);
+        ResetAndStartArrowTween();
+    }
+
+    private void ResetAndStartArrowTween()
+    {
+        LeanTween.cancel(arrowMover.id);
+        ResetArrow();
+        arrowMover = LeanTween.value(gameObject, SetArrowHeigth, 0, screenHeigth, secondPerArrow)
+            .setRepeat(-1)
+            .setOnComplete(ResetArrow);
     }
 
     private void SetArrowHeigth(float heigth)
@@ -104,6 +142,11 @@ public class DDRBody : GunBody
     private void SetFlashFactor(float factor)
     {
         ddrMaterial.SetFloat("_FlashFactor", factor);
+    }
+
+    private void SetBackgroundZoom(float zoom)
+    {
+        ddrMaterial.SetVector("_BackgroundZoom",new Vector4(zoom, zoom, 0, 0));
     }
 
     private float ArrowDirectionToDegrees(int direction)
