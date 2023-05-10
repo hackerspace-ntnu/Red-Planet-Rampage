aeeda20914fd007511c3f535ada07cf436dee759
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-05-10 02:28:10 +0200

    Add dance reload, update dance stats, fix readme formatting
    - Implemented functional reload, stil missing proper visual feedback
    - Stats and statinfo updated to properly add augment to game
    - Fixed a formatting error in readme that would not show build workaround

--- Assets/Scripts/Augment/AugmentImplementations/DDRBody.cs
+++ Assets/Scripts/Augment/AugmentImplementations/DDRBody.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DDRBody : GunBody
 {
@@ -13,36 +14,114 @@ public class DDRBody : GunBody
 
     private float arrowHeigth = 0;
 
-    private float arrowRotationDegrees = 0;
+    private int arrowDirectionQuadrant = 0;
+
+    private const float screenHeigth = 3.5f;
+    private const float errorMarginInput = 0.1f;
+
+    private const float targetPoint = 3;
+    private const float rangeMeh = 0.5f;
+    private const float rangeGood = 0.25f;
+    private const float rangeGreat = 0.1f;
+    private const float rangeExcellent = 0.05f;
+
+    [SerializeField]
+    private float secondPerBeat = 1;
+
 
     public override void Start()
     {
         meshRenderer.materials[screenMaterialIndex] = Instantiate(meshRenderer.materials[screenMaterialIndex]);
         ddrMaterial = meshRenderer.materials[screenMaterialIndex];
 
-        // TODO: Function instead of logic in fixedUpdate, invoked by sound clip play events
+        gunController = transform.parent.GetComponent<GunController>();
+        if (!gunController)
+        {
+            Debug.Log("DanceBody not attached to gun parent!");
+            return;
+        }
+
+        if (gunController.player)
+        {
+            gunController.player.inputManager.onMovePerformed += ArrowSelect;
+            ResetArrow();
+            LeanTween.value(gameObject, SetArrowHeigth, 0, screenHeigth, secondPerBeat)
+                .setRepeat(-1)
+                .setOnComplete(() => ResetArrow());
+        }
+        
     }
 
     protected override void Reload(GunStats stats)
     {
-        // TODO: lerp flash
-        ddrMaterial.SetFloat("_FlashFactor", 5);
-        base.Reload(stats);
-        ddrMaterial.SetFloat("_FlashFactor", 0);
+        float danceAccuracy = 0;
+        if (Mathf.Abs(arrowHeigth - targetPoint) <= rangeExcellent)
+            danceAccuracy = 1;
+        else if(Mathf.Abs(arrowHeigth - targetPoint) <= rangeGreat)
+            danceAccuracy = 0.75f;
+        else if (Mathf.Abs(arrowHeigth - targetPoint) <= rangeGood)
+            danceAccuracy = 0.5f;
+        else if (Mathf.Abs(arrowHeigth - targetPoint) <= rangeMeh)
+            danceAccuracy = 0.25f;
+
+        if (danceAccuracy == 0)
+            return;
+
+        gunController.Reload(reloadEfficiencyPercentage*danceAccuracy);
     }
 
-    private void FixedUpdate()
+    private void ArrowSelect(InputAction.CallbackContext ctx)
     {
-        // TODO: LeanTween.value event
-        arrowHeigth += 0.01f;
-        if (arrowHeigth > 3.5f)
+        switch (arrowDirectionQuadrant)
         {
-            arrowHeigth = 0;
-            arrowRotationDegrees = Mathf.Round(Random.value * 4) * 90;
-            ddrMaterial.SetFloat("_ArrowRotationDegrees", arrowRotationDegrees);
-
+            case 0:
+                if (!(gunController.player.inputManager.moveInput.y > 1 - errorMarginInput))
+                    return;
+                break;
+            case 1:
+                if (!(gunController.player.inputManager.moveInput.x > 1 - errorMarginInput))
+                    return;
+                break;
+            case 2:
+                if (!(gunController.player.inputManager.moveInput.y < -1 + errorMarginInput))
+                    return;
+                break;
+            case 3:
+                if (!(gunController.player.inputManager.moveInput.x < -1 + errorMarginInput))
+                    return;
+                break;
         }
-        ddrMaterial.SetFloat("_ArrowYPos", arrowHeigth);
+
+        Reload(gunController.stats);
     }
 
+    private void SetArrowHeigth(float heigth)
+    {
+        ddrMaterial.SetFloat("_ArrowYPos", heigth);
+        arrowHeigth = heigth;
+    }
+
+    private void SetFlashFactor(float factor)
+    {
+        ddrMaterial.SetFloat("_FlashFactor", factor);
+    }
+
+    private float ArrowDirectionToDegrees(int direction)
+    {
+        // Default orientation (0 degrees) = 12 O' clock
+        return direction*90;
+    }
+
+    private void ResetArrow()
+    {
+        arrowHeigth = 0;
+        arrowDirectionQuadrant = Random.Range(0,4);
+        ddrMaterial.SetFloat("_ArrowRotationDegrees", ArrowDirectionToDegrees(arrowDirectionQuadrant));
+    }
+
+    private void OnDestroy()
+    {
+        if (gunController)
+            gunController.player.inputManager.onMovePerformed -= ArrowSelect;
+    }
 }
