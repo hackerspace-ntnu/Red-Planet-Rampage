66601f2241b980b9f3f99c27e80bce6385c337e8
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-08-01 23:02:33 +0200

    Add basic animation for revolver body reloading

---
+++ Assets/Scripts/Augment/AugmentImplementations/Revolver.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolver : GunBody
+{
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private ParticleSystem steamParticles;
+
+    private GunBarrel barrel;
+    private GunExtension extension;
+    protected override void Reload(GunStats stats)
+    {
+        if (gunController.stats.Ammo >= 1)
+            return;
+
+        barrel = gunController.gameObject.GetComponentInChildren<GunBarrel>();
+        if (barrel)
+            barrel.transform.SetParent(attachmentSite, true);
+            
+
+        extension = gunController.gameObject.GetComponentInChildren<GunExtension>();
+        if (extension)
+            extension.transform.SetParent(attachmentSite, true);
+
+
+        animator.SetTrigger("Reload");
+    }
+
+    public void TriggerSteam()
+    {
+        steamParticles.Play();
+    }
+
+    public void ResetReload()
+    {
+        if (barrel)
+            barrel.transform.SetParent(gunController.transform, true);
+        if (extension)
+            extension.transform.SetParent(gunController.transform, true);
+
+        gunController.Reload(reloadEfficiencyPercentage);
+    }
+
+}
