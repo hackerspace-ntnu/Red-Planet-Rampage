1ce230135e068794179133c94a53cc995f1398f8
Karo Mahmoud <116388350+karofmah@users.noreply.github.com>
2023-10-10 14:03:54 +0200

    Created and added script to platform for vertical movement

---
+++ Assets/Scripts/Platforms/PlatformMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+[SerializeField] private List<Transform> routepoints;
+[SerializeField] private float moveSpeed = 5f;
+private int nextRoutepointIndex;
+
+private void Start()
+{
+    if (routepoints.Count <= 0){
+        Debug.LogError("No waypoints specified");
+        return;
+    } 
+    nextRoutepointIndex = 0;
+}
+
+private void FixedUpdate() {
+    
+    MovePlatform();
+}
+private void MovePlatform()
+{
+  
+    transform.position = Vector3.MoveTowards(transform.position, routepoints[nextRoutepointIndex].transform.position,
+        (moveSpeed * Time.deltaTime));
+
+    if (Vector3.Distance(routepoints[nextRoutepointIndex].transform.position, transform.position) <= 0)
+    {
+        nextRoutepointIndex++;
+    }
+
+    if (nextRoutepointIndex != routepoints.Count) return;
+    routepoints.Reverse();
+    nextRoutepointIndex = 0;
+}
+
+}
