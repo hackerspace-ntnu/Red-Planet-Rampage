6e5de17a6cd088e250917cec0a17f5f5053b27ac
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-10-12 02:35:00 +0200

    Add auctioneer audio
    - Add multiple voicelines for 4 different scenarios
    - Add 2 voice scenarios: bid and sell

--- Assets/Scripts/Auction/Auctioneer.cs
+++ Assets/Scripts/Auction/Auctioneer.cs
@@ -2,32 +2,53 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CollectionExtensions;
 
 public class Auctioneer : MonoBehaviour
 {
     [SerializeField]
     private Animator animator;
+    [Header("Audio")]
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip[] bid;
+    [SerializeField]
+    private AudioClip[] haste;
+    [SerializeField]
+    private AudioClip[] missingChips;
+    [SerializeField]
+    private AudioClip[] sold;
+
 
     public void BidOn(int platformIndex)
     {
+        audioSource.Stop();
         switch (platformIndex)
         {
             case 0:
                 animator.SetTrigger("Auction1Bid");
+                audioSource.clip = bid.RandomElement();
                 break;
             case 1:
                 animator.SetTrigger("Auction2Bid");
+                audioSource.clip = bid.RandomElement();
                 break;
             case 2:
                 animator.SetTrigger("Auction3Bid");
+                audioSource.clip = bid.RandomElement();
                 break;
             default:
                 throw new ArgumentException("No auctioneer animation for given platform index");
         }
+        audioSource.Play();
     }
 
     public void Sell()
     {
         animator.SetTrigger("Sell");
+        audioSource.Stop();
+        audioSource.clip = sold.RandomElement();
+        audioSource.Play();
     }
 }
