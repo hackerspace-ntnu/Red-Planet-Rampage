c5a3d9cf88d19861ec64c62f8f5a2d8ae2049765
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-10-26 15:59:25 +0200

    Add working jiggleMesh!

--- Assets/Scripts/Utils/JiggleMesh.cs
+++ Assets/Scripts/Utils/JiggleMesh.cs
@@ -24,33 +24,30 @@ public class JiggleMesh : MonoBehaviour
     private void LateUpdate()
     {
         // Momentum
-        Vector3 deltaPosition = (oldPosition - transform.position) * 100;// + (animationTarget + transform.up));
+        Vector3 deltaPosition = (oldPosition - transform.position) * 100;
         deltaPosition = new Vector3(Mathf.Clamp(deltaPosition.x, -50, 50), Mathf.Clamp(deltaPosition.y, -50, 50), Mathf.Clamp(deltaPosition.z, -50, 50));
         deltaPosition = Vector3.Slerp(oldDeltaPosition, deltaPosition, Time.deltaTime * 1f);
-        //Debug.DrawRay(transform.position, deltaPosition, Color.white);
+        Debug.DrawRay(transform.position, deltaPosition, Color.white);
 
         // Adjust animationTarget based on momentum loss
         if (oldDeltaPosition.magnitude > deltaPosition.magnitude)
         {
-            deltaAnimationTarget = Vector3.Slerp(deltaAnimationTarget, transform.up, 0.01f*Time.deltaTime);
-            reflectedAnimationTarget = Vector3.Slerp(reflectedAnimationTarget, transform.up, 0.01f * Time.deltaTime);
+            deltaAnimationTarget = Vector3.Slerp(deltaAnimationTarget, transform.up, Time.deltaTime);
+            reflectedAnimationTarget = Vector3.Slerp(reflectedAnimationTarget, transform.up,  Time.deltaTime);
             animationTarget = Vector3.Slerp(deltaAnimationTarget, reflectedAnimationTarget, (1+Mathf.Sin(deltaPosition.magnitude*10))*0.5f);
         }
         else
         {
-            deltaAnimationTarget = deltaPosition;
+            deltaAnimationTarget = transform.up - deltaPosition;
             animationTarget = deltaAnimationTarget.normalized;
             reflectedAnimationTarget = Vector3.Reflect(animationTarget, Vector3.Cross(Vector3.Cross(deltaPosition, previousPosition), transform.up)).normalized;
         }
-        //Debug.DrawRay(Vector3.zero, animationTarget+transform.position, Color.blue);
-        //Debug.DrawRay(transform.position, reflectedAnimationTarget, Color.green);
 
-        // Start rotating towards momentum Target
+        // Start rotating towards AnimationTarget
         Vector3 animatedPosition = Vector3.Slerp(previousPosition, animationTarget, Time.deltaTime * elasticity);
-        //Debug.DrawRay(transform.position, animatedPosition, Color.red);
 
         // Pass state variables
-        jiggleMat.SetVector("_Distance", Quaternion.Inverse(transform.rotation) * animatedPosition.normalized);
+        jiggleMat.SetVector("_Distance", Quaternion.Inverse(transform.rotation) * -animatedPosition);
         previousPosition = animatedPosition;
         oldPosition = transform.position;
         oldDeltaPosition = deltaPosition;
