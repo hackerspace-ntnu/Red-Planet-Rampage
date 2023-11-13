d52c91aad74cd411ea2ddb0c6f25e80870941df9
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-11-13 22:19:12 +0100

    Add ricochet motion to hats.

--- Assets/Scripts/Augment/AugmentImplementations/Hat/HatBarrel.cs
+++ Assets/Scripts/Augment/AugmentImplementations/Hat/HatBarrel.cs
@@ -34,6 +34,8 @@ public class HatBarrel : ProjectileController
     [SerializeField]
     private VisualEffect hatVfx;
 
+    [SerializeField]
+    private float maxLengthBeforeStuck = 100;
     protected override void Awake()
     {
         base.Awake();
@@ -136,13 +138,23 @@ public class HatBarrel : ProjectileController
 
         if (collisions.Length > 0)
         {
-            state.active = false;
-            OnColliderHit?.Invoke(collisions[0], ref state);
             HitboxController hitbox = collisions[0].GetComponent<HitboxController>();
-
             if (hitbox != null)
             {
+                OnColliderHit?.Invoke(collisions[0], ref state);
                 OnHitboxCollision?.Invoke(hitbox, ref state);
+                state.active = false;
+                return;
+            }
+            if (state.distanceTraveled < maxLengthBeforeStuck)
+            {
+                Physics.Raycast(state.oldPosition, state.direction, out RaycastHit hitInfo);
+                state.direction = Vector3.Reflect(state.direction, hitInfo.normal);
+            }
+            else
+            {
+                OnColliderHit?.Invoke(collisions[0], ref state);
+                state.active = false;
             }
         }
     }
