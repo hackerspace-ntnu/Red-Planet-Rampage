9bd40ab1b09c6a842481f0f8ddb6499e90de99eb
Tore Bergebakken <tore.bergebakken@gmail.com>
2023-11-28 20:37:33 +0100

    Make exploding barrels leave decals too

--- Assets/Scripts/Interactables/ExplodingBarrel.cs
+++ Assets/Scripts/Interactables/ExplodingBarrel.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ExplodingBarrel : MonoBehaviour
 {
+    [SerializeField] private MeshRenderer barrelMesh;
 
-    [SerializeField]
-    private MeshRenderer barrelMesh;
+    [SerializeField] private DecalProjector explosionMark;
 
-    void Start()
+    [SerializeField] private LayerMask explosionMarkMask;
+
+    [SerializeField] private float explosionMarkLifeTime = 60;
+
+    private void Start()
     {
         GetComponent<HealthController>().onDeath += Explode;
     }
@@ -16,6 +21,19 @@ public class ExplodingBarrel : MonoBehaviour
         barrelMesh.enabled = false;
         GetComponentInChildren<CapsuleCollider>().enabled = false;
         GetComponent<ExplosionController>().Explode(info.sourcePlayer);
+        LeaveMark();
         Destroy(gameObject, 4);
+    }
+
+    private void LeaveMark()
+    {
+        if (!Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 100, explosionMarkMask))
+            return;
+
+        var position = hit.point + .5f * explosionMark.size.z * hit.normal;
+        var spawnedDecal = Instantiate(explosionMark, position, Quaternion.LookRotation(-hit.normal));
+        spawnedDecal.transform.parent = hit.collider.transform;
+
+        Destroy(spawnedDecal, explosionMarkLifeTime);
     }
 }
