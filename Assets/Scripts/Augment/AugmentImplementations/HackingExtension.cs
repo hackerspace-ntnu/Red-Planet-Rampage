d6866690205c268a95416e7533a718edd8ebad4b
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-12-07 01:49:37 +0100

    Add scrolling hacking material on fire
    - Add and change some wanted scorboard phrases

---
+++ Assets/Scripts/Augment/AugmentImplementations/HackingExtension.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackingExtension : GunExtension
+{
+    [SerializeField]
+    private MeshRenderer meshRenderer;
+    [SerializeField]
+    private Material hackingScreen;
+    [SerializeField]
+    private float scrollAmount = 0.10f;
+    private const int hackingScreenMaterialIndex = 1;
+
+    private GunController gunController;
+
+    private void Start()
+    {
+        meshRenderer.materials[hackingScreenMaterialIndex] = Instantiate(meshRenderer.materials[hackingScreenMaterialIndex]);
+        hackingScreen = meshRenderer.materials[hackingScreenMaterialIndex];
+        gunController = transform.parent.GetComponent<GunController>();
+        if (!gunController)
+        {
+            Debug.Log("HackingExtension not attached to gun parent!");
+            return;
+        }
+        gunController.onFire += Fire;
+    }
+
+    private void Fire(GunStats stats)
+    {
+        hackingScreen.SetFloat("_ScrollAmount", hackingScreen.GetFloat("_ScrollAmount") + scrollAmount);
+    }
+}
