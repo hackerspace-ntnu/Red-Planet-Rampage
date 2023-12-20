bfb5a9e7ad475a8784cecacf7fcbe3194ffa2f8c
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2023-12-20 14:07:04 +0100

    Refactor revolver animation so it also works when decoupled

---
+++ Assets/Scripts/Augment/AugmentImplementations/RevolverAnimator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevolverAnimator : AugmentAnimator
+{
+    private Animator animator;
+    private bool canReload = false;
+    [SerializeField]
+    private Transform attachmentSite;
+    private GunBarrel barrel;
+    private GunExtension extension;
+    public override void OnFire(GunStats stats)
+    {
+
+    }
+
+    public override void OnInitialize(GunStats stats)
+    {
+        animator = GetComponent<Animator>();
+        var playerHand = GetComponentInChildren<PlayerHand>(includeInactive: true);
+        playerHand.transform.SetParent(null);
+        Destroy(playerHand);
+        if (!animator)
+            Debug.Log("Revolver Body missing Animator");
+    }
+
+    public override void OnReload(GunStats stats)
+    {
+        canReload = !canReload;
+        if (!canReload)
+            return;
+
+        barrel = transform.parent.gameObject.GetComponentInChildren<GunBarrel>();
+        if (barrel)
+            barrel.transform.SetParent(attachmentSite, true);
+
+        extension = transform.parent.gameObject.GetComponentInChildren<GunExtension>();
+        if (extension)
+            extension.transform.SetParent(attachmentSite, true);
+        Debug.Log("Reload called");
+        animator.SetTrigger("Reload");
+    }
+}
