214e7de1efb69b1dd3d8ce3c70bf07e035c76c5a
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-01-27 19:37:32 +0100

    Add jigglebone to FPS weapon for movement feedback

--- Assets/Scripts/Utils/JiggleBone.cs
+++ Assets/Scripts/Utils/JiggleBone.cs
@@ -5,38 +5,46 @@ using UnityEngine;
 public class JiggleBone : MonoBehaviour
 {
     [SerializeField]
-    private AnimationCurve wobbleInterpolationCurve;
-    [SerializeField]
-    private float jiggleCoefficient = 20f;
-    [SerializeField]
-    private float maxPositionDelta = 2f;
-    [SerializeField]
-    private float maxRotationDegrees = 100f;
+    float offsetMagnitude = 0.1f;
 
-    private Vector3 previousPosition;
-    private Vector3 targetPosition;
-    private Vector3 animationPosition;
-    private Quaternion previousRotation;
-    private Quaternion targetRotation;
-    private Quaternion animationRotation;
+    Vector3 lastPos = Vector3.zero;
+    Vector3 lastLastPos = Vector3.zero;
 
-    void Awake()
+    [SerializeField]
+    float velocityDamping = 0.1f;
+    [SerializeField]
+    float springStrength = 0.1f;
+
+    [SerializeField]
+    Transform meassurementPoint = null;
+
+    [SerializeField]
+    Transform offsetTarget = null;
+
+    public Rigidbody body;
+
+    private float lastPlayerDiff = 0f;
+
+    private void FixedUpdate()
     {
-        previousPosition = transform.position;
-        previousRotation = transform.rotation;
-    }
+        Vector3 offsetVelocity = (lastPos - lastLastPos) / Time.fixedDeltaTime;
 
-    private void LateUpdate()
-    {
-        animationPosition = transform.position;
-        animationRotation = transform.rotation;
-        targetPosition = Vector3.Slerp(previousPosition, transform.position, Time.deltaTime * jiggleCoefficient);
-        targetRotation = Quaternion.Slerp(previousRotation, transform.rotation, Time.deltaTime * jiggleCoefficient);
+        offsetVelocity -= offsetVelocity * velocityDamping * Time.fixedDeltaTime;
 
-        transform.position = Vector3.MoveTowards(animationPosition, targetPosition, maxPositionDelta);
-        transform.rotation = Quaternion.RotateTowards(animationRotation, targetRotation, maxRotationDegrees);
+        offsetVelocity += (meassurementPoint.position - lastPos) * springStrength * Time.fixedDeltaTime;
 
-        previousPosition = transform.position;
-        previousRotation = transform.rotation;
+        lastLastPos = lastPos;
+
+        lastPos += offsetVelocity * Time.fixedDeltaTime;
+        if ((lastPos - meassurementPoint.position).magnitude > offsetMagnitude)
+        {
+            Vector3 diff = lastPos - meassurementPoint.position;
+            float playerDiff = 0f;
+            if (body)
+                playerDiff = Mathf.Abs(lastPlayerDiff - body.velocity.magnitude);
+            lastPos = meassurementPoint.position + diff.normalized * offsetMagnitude;
+            lastPlayerDiff = playerDiff;
+        }
+        offsetTarget.position = lastPos;
     }
 }
