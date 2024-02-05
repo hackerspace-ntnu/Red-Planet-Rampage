f8cc0694754ff3b60b3ef4b582fd14bdfba5207d
Fredrik Eiding <54811121+Fueredoriku@users.noreply.github.com>
2024-02-05 19:52:49 +0100

    Feature/new player select menu (#463)
    * Add new playerSelect Menu
    
    * Reconnect playerselect menu to start button
    
    * Rebase dev into feature/new-player-select-menu
    
    * Reconnect playerselect menu to start button
    
    * Add finished player select animations
    
    * Add adjustable time between animations
    
    * Fix UI overlay for new player select menu
    
    * Fix bug in CardPeek animation
    
    * Add new animations and method for controlling coroutine
    
    * Fix defaultIdle animation missing
    
    * Resolve changes suggested in PR-review
    
    ---------
    
    Co-authored-by: chrisjk <chrisjk@stud.ntnu.no>

---
+++ Assets/Scripts/PlayerSelect/PlayerEyeManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerEyeManager : MonoBehaviour
+{
+
+    private Material eyeMaterial;
+
+    public void Start()
+    {
+        // Find eye material
+        SkinnedMeshRenderer eyeRenderer = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>();
+        eyeMaterial = eyeRenderer.materials[1];
+        resetEyePosition();
+    }
+
+    /// <summary>
+    /// Resets eye position to default. Looking straight ahead.
+    /// </summary>
+    public void resetEyePosition()
+    {
+        eyeMaterial.SetFloat("_LeftUpperLid", 0.12f);
+        eyeMaterial.SetFloat("_LeftLowerLid", 0.2f);
+        eyeMaterial.SetFloat("_RightUpperLid", 0.12f);
+        eyeMaterial.SetFloat("_RightLowerLid", 0.2f);
+
+        eyeMaterial.SetVector("_LeftEye", new Vector4(0, -0.5f));
+        eyeMaterial.SetVector("_RightEye", new Vector4(0, -0.5f));
+
+        eyeMaterial.SetFloat("_Angery", 0.2f);
+    }
+
+    /// <summary>
+    /// Moves the eyes to the players left.
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    public void lookLeft(float targetPosition)
+    {
+        eyeMaterial.SetVector("_RightEye", new Vector4(targetPosition, 0.0f));
+        eyeMaterial.SetVector("_LeftEye", new Vector4(targetPosition, 0.0f));
+
+    }
+
+    /// <summary>
+    /// Moves the eyes to the players left and a little bit down.
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    public void peekLeft(float targetPosition)
+    {
+        eyeMaterial.SetVector("_RightEye", new Vector4(targetPosition, -0.3f));
+        eyeMaterial.SetVector("_LeftEye", new Vector4(targetPosition, -0.3f));
+
+    }
+
+    /// <summary>
+    /// Moves the eyes to the players right.
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    public void lookRight(float targetPosition)
+    {
+        eyeMaterial.SetVector("_RightEye", new Vector4(-targetPosition, 0.0f));
+        eyeMaterial.SetVector("_LeftEye", new Vector4(-targetPosition, 0.0f));
+    }
+
+    /// <summary>
+    /// Moves the eyes to the players right and a little bit down.
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    public void peekRight(float targetPosition)
+    {
+        eyeMaterial.SetVector("_RightEye", new Vector4(-targetPosition, -0.3f));
+        eyeMaterial.SetVector("_LeftEye", new Vector4(-targetPosition, -0.3f));
+    }
+
+    /// <summary>
+    /// Sets the tilt of players eyelid. Positive value = anger, negative value = sad times.
+    /// </summary>
+    /// <param name="angery"></param>
+    public void setAngery(float angery)
+    {
+        eyeMaterial.SetFloat("_Angery", angery);
+    }
+}
