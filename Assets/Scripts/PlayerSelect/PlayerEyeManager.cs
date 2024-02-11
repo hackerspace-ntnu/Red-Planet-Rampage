27aac3af95fa996a5942876c3758d5bc0746fbff
ChrisKrane <114220928+ChrisKrane@users.noreply.github.com>
2024-02-11 13:42:47 +0100

    Enhancement/new player select room (#487)
    * Update Player Select PokerRoom model with materials
    
    * Update PokerRoom WoodMaterial
    
    * Fix player eye movement after player texture update
    
    * Add cards on table and refactor PlayerEyeMovement
    
    * Fix all players active in playerselect

--- Assets/Scripts/PlayerSelect/PlayerEyeManager.cs
+++ Assets/Scripts/PlayerSelect/PlayerEyeManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -13,14 +15,15 @@ public class PlayerEyeManager : MonoBehaviour
     {
         // Find eye material
         SkinnedMeshRenderer eyeRenderer = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>();
-        eyeMaterial = eyeRenderer.materials[1];
-        resetEyePosition();
+        eyeMaterial = eyeRenderer.materials.Single(material => material.name == "PlayerEyesMaterial (Instance)");
+        
+        ResetEyePosition();
     }
 
     /// <summary>
     /// Resets eye position to default. Looking straight ahead.
     /// </summary>
-    public void resetEyePosition()
+    public void ResetEyePosition()
     {
         eyeMaterial.SetFloat("_LeftUpperLid", 0.12f);
         eyeMaterial.SetFloat("_LeftLowerLid", 0.2f);
@@ -37,7 +40,7 @@ public class PlayerEyeManager : MonoBehaviour
     /// Moves the eyes to the players left.
     /// </summary>
     /// <param name="targetPosition"></param>
-    public void lookLeft(float targetPosition)
+    public void LookLeft(float targetPosition)
     {
         eyeMaterial.SetVector("_RightEye", new Vector4(targetPosition, 0.0f));
         eyeMaterial.SetVector("_LeftEye", new Vector4(targetPosition, 0.0f));
@@ -48,7 +51,7 @@ public class PlayerEyeManager : MonoBehaviour
     /// Moves the eyes to the players left and a little bit down.
     /// </summary>
     /// <param name="targetPosition"></param>
-    public void peekLeft(float targetPosition)
+    public void PeekLeft(float targetPosition)
     {
         eyeMaterial.SetVector("_RightEye", new Vector4(targetPosition, -0.3f));
         eyeMaterial.SetVector("_LeftEye", new Vector4(targetPosition, -0.3f));
@@ -59,7 +62,7 @@ public class PlayerEyeManager : MonoBehaviour
     /// Moves the eyes to the players right.
     /// </summary>
     /// <param name="targetPosition"></param>
-    public void lookRight(float targetPosition)
+    public void LookRight(float targetPosition)
     {
         eyeMaterial.SetVector("_RightEye", new Vector4(-targetPosition, 0.0f));
         eyeMaterial.SetVector("_LeftEye", new Vector4(-targetPosition, 0.0f));
@@ -69,7 +72,7 @@ public class PlayerEyeManager : MonoBehaviour
     /// Moves the eyes to the players right and a little bit down.
     /// </summary>
     /// <param name="targetPosition"></param>
-    public void peekRight(float targetPosition)
+    public void PeekRight(float targetPosition)
     {
         eyeMaterial.SetVector("_RightEye", new Vector4(-targetPosition, -0.3f));
         eyeMaterial.SetVector("_LeftEye", new Vector4(-targetPosition, -0.3f));
@@ -79,7 +82,7 @@ public class PlayerEyeManager : MonoBehaviour
     /// Sets the tilt of players eyelid. Positive value = anger, negative value = sad times.
     /// </summary>
     /// <param name="angery"></param>
-    public void setAngery(float angery)
+    public void SetAngery(float angery)
     {
         eyeMaterial.SetFloat("_Angery", angery);
     }
