0c484cc89bcc04f4e8a6e73932628a202e8951bd
Tore Bergebakken <tore.bergebakken@gmail.com>
2024-02-16 19:23:17 +0100

    Add grace time after ready state in item select
    Also improve responsiveness for keyboard players

--- Assets/Scripts/Control&Input/ItemSelectManager.cs
+++ Assets/Scripts/Control&Input/ItemSelectManager.cs
@@ -1,10 +1,15 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class ItemSelectManager : MonoBehaviour
 {
+    [SerializeField] private float graceTime = 1;
+
     private List<ItemSelectMenu> itemSelectMenus;
+
+    private Coroutine waitRoutine;
 
     public void StartTrackingMenus()
     {
@@ -13,6 +18,7 @@ public class ItemSelectManager : MonoBehaviour
         {
             itemSelectMenus.Add(menu);
             menu.OnReady += OnReady;
+            menu.OnNotReady += OnNotReady;
         }
     }
 
@@ -22,11 +28,26 @@ public class ItemSelectManager : MonoBehaviour
         AuctionDriver.Singleton.ChangeScene();
     }
 
+    private IEnumerator WaitAndFinish()
+    {
+        yield return new WaitForSeconds(graceTime);
+        Finish();
+    }
+
+    private void OnNotReady(ItemSelectMenu menu)
+    {
+        if (waitRoutine is not null)
+        {
+            StopCoroutine(waitRoutine);
+        }
+    }
+
     private void OnReady(ItemSelectMenu menu)
     {
-        if (itemSelectMenus.All(m => m.IsReady))
+        var allPlayersAreReady = itemSelectMenus.All(m => m.IsReady);
+        if (allPlayersAreReady)
         {
-            Finish();
+            waitRoutine = StartCoroutine(WaitAndFinish());
         }
     }
 }
