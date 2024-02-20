45b82f68a6d46c3c87309b3746af6499137f53e7
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-02-20 02:40:27 +0100

    Add manual ai movement for nearby players, fixes #459

--- Assets/Scripts/Control&Input/AIMovement.cs
+++ Assets/Scripts/Control&Input/AIMovement.cs
@@ -3,23 +3,59 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIMovement : PlayerMovement
 {
-    void Start()
-    {
-        body = GetComponent<Rigidbody>();
-        hitbox = GetComponent<BoxCollider>();
-        strafeForce = strafeForceGrounded;
-    }
+    public Transform Target;
+    private NavMeshAgent agent;
+    private float timeSinceJump = 0f;
+
     private void Update()
     {
         UpdateAnimatorParameters();
+        if (!Target)
+            return;
+        transform.LookAt(new Vector3(Target.position.x, transform.position.y, Target.position.z), transform.up);
+        timeSinceJump += Time.deltaTime;
+        if (timeSinceJump >= 3f)
+        {
+            timeSinceJump = timeSinceJump % 3f;
+            if (Physics.Raycast(transform.position, Target.position, ignoreMask) && !StateIsAir)
+                this.enabled = false;
+            Jump();
+        }
     }
 
-    // TODO: Strafe, jump, and keep distance from target player #459
     private void FixedUpdate()
     {
+        if (!Target)
+            return;
+        if (Vector3.Distance(Target.position, transform.position) > 10)
+        {
+            this.enabled = false;
+            return;
+        }
+        var direction = (Target.position - transform.position).normalized;
+        UpdatePosition(new Vector3(direction.x, 0, direction.z));
+    }
+
+    private void OnEnable()
+    {
+        body = GetComponent<Rigidbody>();
+        body.isKinematic = false;
+        hitbox = GetComponent<BoxCollider>();
+        hitbox.isTrigger = false;
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false;
+        strafeForce = strafeForceGrounded;
+    }
+
+    private void OnDisable()
+    {
+        body.isKinematic = true;
+        hitbox.isTrigger = true;
+        agent.enabled = true;
     }
 
     public void Jump()
