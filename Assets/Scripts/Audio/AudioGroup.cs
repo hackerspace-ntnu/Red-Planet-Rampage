daada81addb99f6b4071d83796f3410f1018b96a
Tore Bergebakken <tore.bergebakken@gmail.com>
2024-03-12 21:16:52 +0100

    Use audiogroup for explosion sfx
    and wire them into the correct mixer for proper volume!

--- Assets/Scripts/Audio/AudioGroup.cs
+++ Assets/Scripts/Audio/AudioGroup.cs
@@ -15,6 +15,9 @@ public class AudioGroup : ScriptableObject
     [SerializeField]
     private IntRange semitoneRange;
 
+    [SerializeField]
+    private bool continuousPitchBend;
+
 #if UNITY_EDITOR
     [MinMax(0, 1), ContextMenuItem("Preview/Low Volume", "PreviewLowVolume"), ContextMenuItem("Preview/High Volume", "PreviewHighVolume")]
 #endif
@@ -27,7 +30,8 @@ public class AudioGroup : ScriptableObject
     }
     public void Play(AudioSource source)
     {
-        source.pitch = Mathf.Pow(SEMITONE_PITCH_CONVERSION_UNIT, Random.Range(semitoneRange.Min, semitoneRange.Max));
+        float pitch = continuousPitchBend ? Random.Range((float)semitoneRange.Min, (float)semitoneRange.Max) : Random.Range(semitoneRange.Min, semitoneRange.Max);
+        source.pitch = Mathf.Pow(SEMITONE_PITCH_CONVERSION_UNIT, pitch);
         source.volume = Random.Range(volumeRange.Min, volumeRange.Max);
         source.PlayOneShot(sounds.RandomElement());
     }
