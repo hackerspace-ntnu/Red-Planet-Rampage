8074ea04c1667edef687f56f1e7f700e6ee73ac5
Fredrik Eiding <54811121+Fueredoriku@users.noreply.github.com>
2024-03-16 10:29:40 +0000

    Basic networking  (#574)
    * Add the netcode for gameobjects package
    
    * Add steamSDK support
    
    * Add multiplayer tools package 
    
    * Add mirror+steamworks.net, enable hosting and joining on steam

---
+++ Assets/Scripts/Control&Input/Peer2PeerTransport.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Mirror;
+using UnityEngine;
+
+public class Peer2PeerTransport : NetworkManager
+{
+    
+}
