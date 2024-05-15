f67ed3b407fd2cd59ddd0556f6a9446585044516
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-05-16 00:54:38 +0200

    Fix build issue with missing resources, deactivate logger

--- Assets/Scripts/Extensions/PortalExtension/PortalExtensionController.cs
+++ Assets/Scripts/Extensions/PortalExtension/PortalExtensionController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.ShaderGraph.Serialization;
 using UnityEngine;
 using UnityEngine.VFX;
 
