3d79634a6e6c42a91ce0fce6e9cbae1094429261
Thomas J. G. <thomasjgabrielsen@gmail.com>
2024-06-01 01:09:06 +0200

    Renamed DropdownController.cs to UIReselector.cs to better define purpose.
    Simplified UIReselector.cs
    Applied changes to Dropdown.prefab

--- Assets/Scripts/UI/UIReselector.cs
+++ Assets/Scripts/UI/UIReselector.cs
@@ -6,20 +6,17 @@ using System.Collections;
 /// <summary>
 /// Reselects unity dropdown elements when you enter a dropdown unity pls y u do dis. Add to every dropdown.
 /// </summary>
-public class DropdownController : MonoBehaviour, ISelectHandler
+public class UIReselector : MonoBehaviour, ISelectHandler
 {
-    private Selectable firstItem;
-
     public void OnSelect(BaseEventData eventData)
-    {
-        firstItem = GetComponentInChildren<Selectable>();
+    {   
         StartCoroutine(WaitAndSelectFirstDropdownItem());
     }
 
     private IEnumerator WaitAndSelectFirstDropdownItem()
     {
         yield return new WaitForEndOfFrame();
-        firstItem.Select();
+        GetComponentInChildren<Selectable>().Select();
     }
 
 }
