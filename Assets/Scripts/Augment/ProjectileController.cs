0139bc552a5c0dfff6475aae34d728d459ae9db1
Tore Bergebakken <tore.bergebakken@gmail.com>
2024-06-09 21:30:08 +0200

    Provide correct aim orientation when shooting

--- Assets/Scripts/Augment/ProjectileController.cs
+++ Assets/Scripts/Augment/ProjectileController.cs
@@ -136,6 +136,7 @@ public abstract class ProjectileController : NetworkBehaviour
 
     protected virtual void Awake()
     {
+        syncDirection = SyncDirection.ClientToServer;
 
         collisionLayers = LayerMask.GetMask("Default", "HitBox");
 
