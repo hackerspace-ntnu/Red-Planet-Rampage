b44c18d4af09509f51a86c9152b6b6c2bf977755
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-06-24 01:04:05 +0200

    Add feedback for shootable plug, add new pullback animation + FX

--- Assets/Scripts/Augment/AugmentImplementations/TetherPlug.cs
+++ Assets/Scripts/Augment/AugmentImplementations/TetherPlug.cs
@@ -9,5 +9,19 @@ public class TetherPlug : MonoBehaviour
     private Transform plugOutput;
     public HealthController Health;
     public Transform WireOrigin => plugOutput;
+    [SerializeField]
+    private Material plugMaterial;
+    [SerializeField]
+    private MeshRenderer meshRenderer;
 
+    private void Start()
+    {
+        meshRenderer.materials[0] = Instantiate(meshRenderer.materials[0]);
+        plugMaterial = meshRenderer.materials[0];
+    }
+
+    public void SetPulseStrength(float value)
+    {
+        plugMaterial.SetFloat("_PulseStrength", value);
+    }
 }
