fb4f145ebbc7d5ff7cd0660f20a90ecaad1994db
Tore Bergebakken <tore.bergebakken@gmail.com>
2024-06-24 22:18:00 +0200

    Add starting weapon customization

--- Assets/Scripts/UI/MainMenu/GamemodeEditor.cs
+++ Assets/Scripts/UI/MainMenu/GamemodeEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -32,25 +33,57 @@ public class GamemodeEditor : MonoBehaviour
     [SerializeField]
     private Slider maxChipsSlider;
 
+    [SerializeField]
+    private TMP_Dropdown startingWeaponDropdown;
+
+    [SerializeField]
+    private TMP_Dropdown startingBodyDropdown;
+
+    [SerializeField]
+    private TMP_Dropdown startingBarrelDropdown;
+
+    [SerializeField]
+    private TMP_Dropdown startingExtensionDropdown;
+
     private Ruleset ruleset;
 
     private void Awake()
     {
+        AddDropdownValues();
         ResetRuleset();
+    }
+
+    private void AddDropdownValues()
+    {
+        startingBodyDropdown.AddOptions(StaticInfo.Singleton.Bodies.Select(b => b.displayName).ToList());
+        startingBarrelDropdown.AddOptions(StaticInfo.Singleton.Barrels.Select(b => b.displayName).ToList());
+        startingExtensionDropdown.AddOptions(new string[] { "None" }.Union(StaticInfo.Singleton.Extensions.Select(b => b.displayName)).ToList());
+        startingBodyDropdown.RefreshShownValue();
+        startingBarrelDropdown.RefreshShownValue();
+        startingExtensionDropdown.RefreshShownValue();
     }
 
     public void ResetRuleset()
     {
         // TODO is this valid clone???
         ruleset = Instantiate(MatchRules.Singleton.CustomRulesTemplate);
+
         winConditionSlider.value = (int)ruleset.MatchWinCondition.WinCondition;
         stopConditionSlider.value = (int)ruleset.MatchWinCondition.StopCondition;
         stopConditionAmountSlider.value = ruleset.MatchWinCondition.AmountForStopCondition;
+
         startingChipsSlider.value = ruleset.StartingChips;
         chipsPerRoundSlider.value = ruleset.ChipsPerRoundPassed;
         chipsPerKillSlider.value = ruleset.ChipsPerKill;
         chipsPerWinSlider.value = ruleset.ChipsPerWin;
         maxChipsSlider.value = ruleset.MaxChips;
+
+        startingWeaponDropdown.value = (int)ruleset.StartingWeapon.Type;
+        startingBodyDropdown.value = StaticInfo.Singleton.Bodies.IndexOf(i => i == ruleset.StartingWeapon.Body);
+        startingBarrelDropdown.value = StaticInfo.Singleton.Barrels.IndexOf(i => i == ruleset.StartingWeapon.Barrel);
+        // -1 should become 0 for the special extension index
+        startingExtensionDropdown.value = 1 + StaticInfo.Singleton.Extensions.IndexOf(i => i == ruleset.StartingWeapon.Extension);
+
         ApplyCondition();
     }
 
@@ -129,5 +162,39 @@ public class GamemodeEditor : MonoBehaviour
     public void SetMaxChips(float value)
     {
         ruleset.MaxChips = Mathf.RoundToInt(value);
+    }
+
+    public void SetStartingWeaponType(int index)
+    {
+        var changed = ruleset.StartingWeapon;
+        changed.Type = (StartingWeaponType)index;
+        ruleset.StartingWeapon = changed;
+
+        var shouldShowWeaponDropdowns = ruleset.StartingWeapon.Type is StartingWeaponType.Specific;
+        startingBodyDropdown.transform.parent.gameObject.SetActive(shouldShowWeaponDropdowns);
+        startingBarrelDropdown.transform.parent.gameObject.SetActive(shouldShowWeaponDropdowns);
+        startingExtensionDropdown.transform.parent.gameObject.SetActive(shouldShowWeaponDropdowns);
+    }
+
+    public void SetStartingBody(int index)
+    {
+        var changed = ruleset.StartingWeapon;
+        changed.Body = StaticInfo.Singleton.Bodies[index];
+        ruleset.StartingWeapon = changed;
+    }
+
+    public void SetStartingBarrel(int index)
+    {
+        var changed = ruleset.StartingWeapon;
+        changed.Barrel = StaticInfo.Singleton.Barrels[index];
+        ruleset.StartingWeapon = changed;
+    }
+
+    public void SetStartingExtension(int index)
+    {
+        var changed = ruleset.StartingWeapon;
+        // Extension "index" starts at 0 for no extension
+        changed.Extension = index == 0 ? null : StaticInfo.Singleton.Extensions[index - 1];
+        ruleset.StartingWeapon = changed;
     }
 }
