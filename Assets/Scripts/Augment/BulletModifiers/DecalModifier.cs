8c0ee69db6a6e8fe3374469e8a709c419d450b68
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-09-27 02:10:51 +0200

    Fix issue where decals would attach to and follow hitboxes

--- Assets/Scripts/Augment/BulletModifiers/DecalModifier.cs
+++ Assets/Scripts/Augment/BulletModifiers/DecalModifier.cs
@@ -23,7 +23,7 @@ public class DecalModifier : MonoBehaviour, ProjectileModifier
 
     [Range(0f, 180f)][SerializeField] private float angleVariation = 180f;
 
-    private const int allGunsAndPlayersMask = (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11) | (1 << 12) | (1 << 13) | (1 << 14) | (1 << 15);
+    private const int allGunsAndPlayersMask = (1 << 3) | (1 << 8) | (1 << 9) | (1 << 10) | (1 << 11) | (1 << 12) | (1 << 13) | (1 << 14) | (1 << 15);
 
     private void Awake()
     {
