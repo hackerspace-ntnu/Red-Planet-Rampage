10f5d5658645576622ff57ea2ab0087af213cdef
Tore Bergebakken <tore.bergebakken@gmail.com>
2024-10-01 00:02:56 +0200

    Fix nullref when soda cans fall down in The Pit

--- Assets/Scripts/Interactables/DeathTriggerScript.cs
+++ Assets/Scripts/Interactables/DeathTriggerScript.cs
@@ -4,9 +4,9 @@ public class DeathTriggerScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.TryGetComponent<HitboxController>(out var hitbox))
+        if (other.TryGetComponent<HitboxController>(out var hitbox) && hitbox.health.TryGetComponent<PlayerManager>(out var player))
         {
-            hitbox.DamageCollider(new DamageInfo(hitbox.health.GetComponent<PlayerManager>(), 1000, hitbox.transform.position, Vector3.zero, DamageType.Falling));
+            hitbox.DamageCollider(new DamageInfo(player, 1000, hitbox.transform.position, Vector3.zero, DamageType.Falling));
         }
     }
 }
