aa1ac6ca26eee5a7af7fae3cb252da2a8bddfe2e
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-11-26 03:23:48 +0100

    Add texture to boom-bardier and remote detonation of dynamites

--- Assets/Scripts/Augment/AugmentImplementations/DynamiteBarrel.cs
+++ Assets/Scripts/Augment/AugmentImplementations/DynamiteBarrel.cs
@@ -1,24 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class DynamiteBarrel : MonoBehaviour
+public class DynamiteBarrel : GunBarrel
 {
-    // Start is called before the first frame update
-    private GunController gunController;
+    private List<StickyDynamite> activeDynamites = new();
+    [SerializeField]
+    private StickyProjectileModifier stickyModifer;
+
     void Start()
     {
         gunController = transform.parent.GetComponent<GunController>();
         if (!gunController || !gunController.Player)
             return;
 
+        gunController.Player.onDeath += OnDeath;
+        stickyModifer.OnStuckToTarget += AddDynamite;
+
+        if (gunController.Player is AIManager)
+        {
+            StartCoroutine(TryDetonating());
+            return;
+        }   
+
+        gunController.Player.GetComponent<PlayerMovement>().ResetZoom();
         gunController.Player.inputManager.onZoomPerformed += OnZoom;
     }
 
+    private void AddDynamite(StuckObject stuckObject)
+    {
+        if (stuckObject is StickyDynamite)
+            activeDynamites.Add((StickyDynamite)stuckObject);
+    }
+
+    // Letting the AIs also explode stuff every now and then
+    private IEnumerator TryDetonating()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            if (activeDynamites.Any())
+                activeDynamites.ForEach(dynamite => dynamite.Detonate(gunController.Player));
+            activeDynamites.Clear();
+        }
+    }
 
     private void OnZoom(InputAction.CallbackContext ctx)
     {
-        
+        activeDynamites.ForEach(dynamite => dynamite.Detonate(gunController.Player));
+        activeDynamites.Clear();
+    }
+
+    private void OnDeath(PlayerManager killer, PlayerManager victim, DamageInfo info)
+    {
+        StopAllCoroutines();
+        if (!gunController || !gunController.Player)
+            return;
+
+        activeDynamites.ForEach(dynamite => dynamite.gameObject.SetActive(false));
+
+        stickyModifer.OnStuckToTarget -= AddDynamite;
+
+        if (gunController.Player is not AIManager)
+            gunController.Player.inputManager.onZoomPerformed -= OnZoom;
+    }
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+        if (!gunController || !gunController.Player)
+            return;
+
+        activeDynamites.ForEach(dynamite => dynamite.gameObject.SetActive(false));
+
+        stickyModifer.OnStuckToTarget -= AddDynamite;
+
+        if (gunController.Player is not AIManager)
+            gunController.Player.inputManager.onZoomPerformed -= OnZoom;
     }
 }
--- Assets/Scripts/Augment/BulletModifiers/StickyProjectileModifier.cs
+++ Assets/Scripts/Augment/BulletModifiers/StickyProjectileModifier.cs
@@ -78,6 +78,9 @@ public class StickyProjectileModifier : MonoBehaviour, ProjectileModifier
 
     private void OnDestroy()
     {
+        if (stuckObjects == null)
+            return;
+
         stuckObjects.Flush();
         stuckObjects = null;
     }
