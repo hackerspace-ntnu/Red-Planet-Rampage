300c01d30a019a67c6a27a604a79f30aed97bdb0
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-11-28 21:34:29 +0100

    Add explosive barrel variant for enlarger

--- Assets/Scripts/Augment/AugmentImplementations/StickyDynamite.cs
+++ Assets/Scripts/Augment/AugmentImplementations/StickyDynamite.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -10,11 +11,26 @@ public class StickyDynamite : StuckObject
     [SerializeField]
     private Renderer mesh;
     [SerializeField]
+    private Renderer explosiveBarrel;
+    [SerializeField]
+    private VisualEffect[] barrelSmoke;
+    [SerializeField]
     private ExplosionController explosion;
+
+    public void SetBarrel()
+    {
+        mesh.enabled = false;
+        explosiveBarrel.enabled = true;
+        smoke.enabled = false;
+        barrelSmoke.ToList().ForEach(smoke => smoke.enabled = true);
+    }
+
     public void Detonate(PlayerManager sourcePlayer)
     {
         mesh.enabled = false;
+        explosiveBarrel.enabled = false;
         smoke.enabled = false;
+        barrelSmoke.ToList().ForEach(smoke => smoke.enabled = false);
         explosion.Explode(sourcePlayer);
         explosion.transform.parent.parent = null;
     }
