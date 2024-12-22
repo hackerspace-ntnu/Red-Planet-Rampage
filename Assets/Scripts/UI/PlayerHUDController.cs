69240de0f614084f09fb077174be56208b56d84d
Fredrik Lillemoen Eiding <fueredoriku@gmail.com>
2024-12-22 05:05:47 +0100

    Add sine wave animation for health HUD

--- Assets/Scripts/UI/PlayerHUDController.cs
+++ Assets/Scripts/UI/PlayerHUDController.cs
@@ -157,7 +157,7 @@ public class PlayerHUDController : MonoBehaviour
         chipBox.anchoredPosition = new Vector2(originalChipX, -originalChipY);
 
         healthBarScaleX = healthBar.transform.localScale.x;
-        healthBar.color = healthMax;
+        healthBar.material.SetColor("_Color", HealthPalette(1f));
         healthTextPosition = healthText.transform.localPosition;
 
         crosshair.rectTransform.sizeDelta *= SettingsDataManager.Singleton.Data.CrosshairSize;
@@ -271,7 +271,7 @@ public class PlayerHUDController : MonoBehaviour
             width = Mathf.Max(width, 0.001f);
 
         LeanTween.value(healthBar.gameObject, SetHealthBar, healthBar.transform.localScale.x, width, tweenDuration);
-        healthBar.color = Color.Lerp(healthMin, healthMax, (Mathf.Max(currentHealth, 0) / maxHealth));
+        healthBar.material.SetColor("_Color", HealthPalette(Mathf.Max(currentHealth, 0) / maxHealth));
     }
 
     private void SetHealthBar(float width)
@@ -405,5 +405,23 @@ public class PlayerHUDController : MonoBehaviour
                 crosshairMaterial.DisableKeyword("_MODE_" + mode.ToString().ToUpper());
             else
                 crosshairMaterial.EnableKeyword("_MODE_" + mode.ToString().ToUpper());
+    }
+
+    // Implementation based of https://iquilezles.org/articles/palettes/
+    // Health is expected to be normalized.
+    public Color HealthPalette(float health)
+    {
+        health = Mathf.SmoothStep(0.25f, 0.68f, health);
+        Vector3 contrast = new Vector3(0.6f, 0.6f, 0f);
+        Vector3 brightness = new Vector3(0.6f, 0.3f, 0.5f);
+        Vector3 oscilations = new Vector3(0.9f, 0.9f, 0f);
+        Vector3 phase = new Vector3(0.69f, 0.35f, 0.6f);
+        Vector3 octave = Mathf.PI * 2f * (oscilations * health + phase);
+        Vector3 rgb = new Vector3(Mathf.Cos(octave.x), Mathf.Cos(octave.y), Mathf.Cos(octave.z));
+        Vector3 palette = contrast + new Vector3(
+            brightness.x * rgb.x,
+            brightness.y * rgb.y,
+            brightness.z * rgb.z);
+        return new Color(palette.x, palette.y, palette.z);
     }
 }
