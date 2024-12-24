05bba21756cb4a4e307c24910400f92c82375109
Tore Bergebakken <toberge@stud.ntnu.no>
2024-12-24 16:47:07 +0100

    Add santa hats

--- Assets/Scripts/Gamestate/PlayerManager.cs
+++ Assets/Scripts/Gamestate/PlayerManager.cs
@@ -101,6 +101,9 @@ public class PlayerManager : NetworkBehaviour
     protected GameObject[] meshBase;
 
     [SerializeField]
+    private GameObject[] accessoryMeshes;
+
+    [SerializeField]
     protected PlayerIK playerIK;
     public PlayerIK PlayerIK => playerIK;
 
@@ -491,6 +494,7 @@ public class PlayerManager : NetworkBehaviour
         // Set correct layer on self, mesh and gun (TODO)
         gameObject.layer = playerLayer;
         meshBase.ToList().ForEach(mesh => SetLayerOnSubtree(mesh, playerLayer));
+        accessoryMeshes.ToList().ForEach(mesh => SetLayerOnSubtree(mesh, playerLayer));
         if (hudController)
             SetLayerOnSubtree(hudController.gameObject, LayerMask.NameToLayer("Gun " + playerIndex));
     }
