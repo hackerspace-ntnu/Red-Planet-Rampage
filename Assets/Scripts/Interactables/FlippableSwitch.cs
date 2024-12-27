7b26f189c5cd93c3c5644a0f12734d11b071c254
Tore Bergebakken <toberge@stud.ntnu.no>
2024-12-27 17:32:53 +0100

    Add flippable switch to Grand Canyon

---
+++ Assets/Scripts/Interactables/FlippableSwitch.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlippableSwitch : MonoBehaviour
+{
+    public delegate void FlipEvent(bool on);
+    public FlipEvent OnFlip;
+
+    [SerializeField]
+    private HealthController health;
+
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private bool isOn = false;
+
+    private void Start()
+    {
+        health.onDamageTaken += OnDamageTaken;
+    }
+
+    private void OnDestroy()
+    {
+        health.onDamageTaken -= OnDamageTaken;
+    }
+
+    private void OnDamageTaken(HealthController healthController, float damage, DamageInfo info)
+    {
+        animator.SetTrigger("Flip");
+        isOn = !isOn;
+        OnFlip?.Invoke(isOn);
+    }
+}
