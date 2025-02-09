a3021e5e541e5515646aff15d70c51eacde5d8a8
Tore Bergebakken <tore.bergebakken@gmail.com>
2025-02-09 20:30:33 +0100

    Make diggers in GC knock players upwards on death

--- Assets/Scripts/Interactables/DeathTriggerScript.cs
+++ Assets/Scripts/Interactables/DeathTriggerScript.cs
@@ -5,13 +5,19 @@ public class DeathTriggerScript : MonoBehaviour
     [SerializeField]
     private bool isAIOnly;
 
+    [SerializeField]
+    private float damage = 1000;
+
+    [SerializeField]
+    private Vector3 knockbackDirection = Vector3.zero;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<HitboxController>(out var hitbox) && hitbox.health.TryGetComponent<PlayerManager>(out var player))
         {
             if (isAIOnly && player is not AIManager)
                 return;
-            hitbox.DamageCollider(new DamageInfo(player, 1000, hitbox.transform.position, Vector3.zero, DamageType.Falling));
+            hitbox.DamageCollider(new DamageInfo(player, damage, hitbox.transform.position, knockbackDirection, DamageType.Falling));
         }
     }
 }
