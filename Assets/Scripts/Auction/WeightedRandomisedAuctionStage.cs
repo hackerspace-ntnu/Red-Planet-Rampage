2f704a40f39da2685b91347a70b2b8470088ef19
Tore Bergebakken <tore.bergebakken@gmail.com>
2025-07-06 21:39:42 +0200

    Weight auctions based on owned items
    Requires preparing weights before draws.

--- Assets/Scripts/Auction/WeightedRandomisedAuctionStage.cs
+++ Assets/Scripts/Auction/WeightedRandomisedAuctionStage.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using CollectionExtensions;
 using UnityEngine;
 
@@ -6,37 +8,113 @@ using UnityEngine;
 public class WeightedRandomisedAuctionStage : RandomisedAuctionStage
 {
     // TODO: the count of rates must mach the count of items - can be solved in onvalidate/in editor script
-    [SerializeField]
-    protected float[] rates;
+    protected List<(Item item, int weight)> weightedItems;
+    protected int totalWeight;
+
+    private Dictionary<Item, int> ownedByXPlayers;
 
 
 #if UNITY_EDITOR
     private void OnValidate()
     {
         numItems = Mathf.Max(1, numItems); // at least one pls
-        if (items.Length != rates.Length)
-        {
-            rates.Resize(items.Length);
-        }
     }
 #endif
 
-    private Item[] RandomSelectionWithReplacement()
+    public void PrepareWeights()
     {
-        Item[] selection = new Item[numItems];
-        for (int i = 0; i < numItems; i++)
-        {
-            selection[i] = items.RandomElement();
-        }
-        return selection;
+        // Technically this shouldn't need to happen for each auction stage
+        // but relatively inexpensive all things considered...
+        ownedByXPlayers = RPRNetworkManager.PlayerInstanceByID.Values
+            .Select(p => p.identity.AllItems.Select(item => (item, owners: 1)))
+            .Aggregate((acc, val) =>
+            {
+                var accSet = acc.Select(pair => pair.item).ToHashSet();
+                var valSet = val.Select(pair => pair.item).ToHashSet();
+                return acc
+                    // Increment count if present in more inventories
+                    .Select(pair => (pair.item, owners: pair.owners + (valSet.Contains(pair.item) ? 1 : 0)))
+                    // and include the new ones
+                    .Concat(val.Where(pair => !accSet.Contains(pair.item)));
+            })
+            .ToDictionary(pair => pair.item, pair => pair.owners);
+
+        weightedItems = items
+            .Select(i => (item: i, weight: DetermineWeight(i)))
+            .ToList();
+
+        totalWeight = weightedItems.Sum(pair => pair.weight);
     }
-    private Item[] RandomSelectionWithoutReplacement()
+
+    private int DetermineWeight(Item item)
     {
-        int[] idx = items.RandomIndicesOf();
+        var score = 1000;
+
+        // Being owned by a player is the biggest sign we should ignore it
+        if (ownedByXPlayers.TryGetValue(item, out var owners))
+        {
+            score -= owners switch
+            {
+                0 => 0,
+                1 => 700,
+                2 => 800,
+                3 => 950,
+                _ => 999,
+            };
+        }
+
+        // TODO check if it has been in previous auction (TODO fix that stuff???)
+
+        // TODO more criteria? winning weapon etc?
+
+        return Mathf.Max(score, 1);
+    }
+
+    // prolly really stupick but this is what my exhausted brain came up with :))))))
+    private Item RandomElement(bool withReplacement = false)
+    {
+        // This should be compared to running weight sum
+        // Larger weight = higher probability of being picked
+        int random = Random.Range(0, totalWeight);
+
+        Item item = null;
+        int weight = 0, index = -1, runningWeight = 0;
+        for (int i = 0; i < weightedItems.Count; i++)
+        {
+            (item, weight) = weightedItems[i];
+            // Random within span?
+            if (random < runningWeight + weight && random >= runningWeight)
+            {
+                // This is it.
+                index = i;
+                break;
+            }
+            runningWeight += weight;
+        }
+
+        if (index < 0)
+        {
+            Debug.LogWarning($"Found no item at {random} running weight of {totalWeight}");
+            // Default to *something* if we don't find an item
+            index = Random.Range(0, weightedItems.Count);
+            (item, weight) = weightedItems[index];
+        }
+
+        if (!withReplacement)
+        {
+            weightedItems.RemoveAt(index);
+            totalWeight -= weight;
+        }
+
+        return item;
+    }
+
+    private Item[] RandomSelection(bool withReplacement = false)
+    {
         Item[] selection = new Item[numItems];
         for (int i = 0; i < numItems; i++)
         {
-            selection[i] = items[idx[i]];
+            selection[i] = RandomElement(withReplacement);
         }
         return selection;
     }
@@ -49,12 +127,7 @@ public class WeightedRandomisedAuctionStage : RandomisedAuctionStage
             return false;
         }
 
-        float sum = 0f;
-        for (int i = 0; i < rates.Length; i++)
-            sum += rates[i];
-
-
-        Item[] selection = withReplacement ? RandomSelectionWithReplacement() : RandomSelectionWithoutReplacement();
+        Item[] selection = RandomSelection(withReplacement);
         round = new BiddingRound(selection);
         return true;
     }
